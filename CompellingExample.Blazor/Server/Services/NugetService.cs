aec44cff6b08a367ec6c354a94d042d4854467d5
Rich Bryant <rich.bryant@live.co.uk>
2020-07-11 22:38:19 +0100

    Made NugetService nicely Functional

--- CompellingExample.Blazor/Server/Services/NugetService.cs
+++ CompellingExample.Blazor/Server/Services/NugetService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CompellingExample.Blazor.Shared.Models;
+using LanguageExt;
 using NuGet.Configuration;
 using NuGet.Protocol.Core.Types;
 
@@ -16,8 +17,15 @@ namespace CompellingExample.Blazor.Server.Services
 
     public class NugetService : INugetService
     {
-        public async Task<IEnumerable<NugetPackageDto>> GetNugetPackages(string term)
+        public async Task<IEnumerable<NugetPackageDto>> GetNugetPackages(string term) =>
+            await NugetFunctions.NuGetLocalRepository()
+                .GetResource()
+                .Bind(x => NugetFunctions.GetMetadata(x, term))
+                .Map(x => x.AsDtos());
+
+        public async Task<IEnumerable<NugetPackageDto>> GetNugetPackagesImperative(string term)
         {
+            var sources = Repository.Provider.GetCoreV3();
             var providers = new List<Lazy<INuGetResourceProvider>>();
             providers.AddRange(Repository.Provider.GetCoreV3());
             var packageSource = new PackageSource("https://api.nuget.org/v3/index.json");
@@ -29,5 +37,24 @@ namespace CompellingExample.Blazor.Server.Services
                 .ConfigureAwait(false);
             return metadata.Select(x => new NugetPackageDto(x));
         }
+    }
+
+    public static class NugetFunctions
+    {
+        public static SourceRepository NuGetLocalRepository()
+            => new SourceRepository(new PackageSource("https://api.nuget.org/v3/index.json"),
+                new List<Lazy<INuGetResourceProvider>>(Repository.Provider.GetCoreV3()));
+
+        public static IEnumerable<NugetPackageDto> AsDtos(this IEnumerable<IPackageSearchMetadata> list)
+            => list.Select(x => x.AsDto());
+
+        public static NugetPackageDto AsDto(this IPackageSearchMetadata nuget)
+            => new NugetPackageDto(nuget);
+
+        public static async Task<PackageSearchResource> GetResource(this SourceRepository repo)
+            => await repo.GetResourceAsync<PackageSearchResource>().ConfigureAwait(false);
+
+        public static async Task<IEnumerable<IPackageSearchMetadata>> GetMetadata(PackageSearchResource source, string term)
+            => await source.SearchAsync(term, new SearchFilter(false), 0, 10, null, new CancellationToken());
     }
 }
